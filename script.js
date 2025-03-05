71761b9196ec535cba23407058ac62a37cb2beef
Selin <ozenselin@users.noreply.github.com>
2025-03-05 17:41:35 +0300

    Add: implement operate function

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function divide(num1, num2){
     return Math.round((num1 / num2) * scale) / scale;
 }
 
+function operate(operator, num1, num2){
+    let result;
+    switch(operator){
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+
+    return result;
+}
